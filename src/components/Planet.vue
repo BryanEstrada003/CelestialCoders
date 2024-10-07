a6587740103107d8b38acc5c14a00147d74535e5
melpgala <melpgala@espol.edu.ec>
2024-10-06 23:10:38 -0500

    Intento tags planetas

--- src/components/Planet.vue
+++ src/components/Planet.vue
@@ -1,7 +1,23 @@
+<template>
+  <group>
+    <mesh :position="planetData.position" :name="planetData.name">
+      <sphereGeometry :args="[1, 32, 32]" />
+      <meshStandardMaterial :color="planetData.color" />
+    </mesh>
+    <text-mesh
+      :text="planetData.name"
+      :fontSize="0.5"
+      color="white"
+      :position="[0, 1.5, 0]"
+    />
+  </group>
+</template>
+
 <script lang="ts">
-import {defineComponent, onMounted, PropType} from 'vue';
-import {PlanetData} from '../types';
-import {usePlanetMesh} from '../uses/usePlanetMesh.ts';
+import { defineComponent, onMounted } from 'vue';
+import { PlanetData } from '../types';
+import { usePlanetMesh } from '../uses/usePlanetMesh.ts';
+import { Text } from 'troika-three-text';
 
 export default defineComponent({
   name: 'Planet',
@@ -12,15 +28,26 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const {createPlanetMesh} = usePlanetMesh();
+    const { createPlanetMesh } = usePlanetMesh();
 
     onMounted(() => {
       createPlanetMesh(props.planetData);
+
+      const textMesh = new Text();
+      textMesh.text = props.planetData.name;
+      textMesh.fontSize = 0.5;
+      textMesh.color = 'white';
+      textMesh.position.set(0, 1.5, 0);
+      
+      // Añadir el texto a la escena
+      textMesh.sync();
     });
 
-    // watch(() => props.planetData, (newData) => {
-    //   updatePlanetMesh(newData);
-    // }, {deep: true});
+    return {};
   }
 });
 </script>
+
+<style scoped>
+/* Estilos para el texto pueden ir aquí si es necesario */
+</style>
